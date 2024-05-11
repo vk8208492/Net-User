7cbab820ecd7ca946b82250648ffe6b018521a20
Vlad <147541054+kvs23031999@users.noreply.github.com>
2024-05-11 18:33:10 +0300

    initial commit

---
+++ src/main/java/app/server/NettyServer.java
@@ -0,0 +1,46 @@
+package app.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+
+public class NettyServer {
+
+    static final int PORT = 8001;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) {
+                    ChannelPipeline p = ch.pipeline();
+
+                    p.addLast(new StringDecoder());
+                    p.addLast(new StringEncoder());
+                    // Обробник на сервері
+                    p.addLast(new ServerHandler());
+                }
+            });
+            ChannelFuture f = b.bind(PORT).sync();
+            System.out.println("Server started and waiting for users...");
+
+            f.channel().closeFuture().sync();
+        } finally {
+
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
